d2287842c408fd8908e33d08b3e6338426d8ac99
Maxim <oxxxyhot@gmail.com>
2021-03-28 20:13:05 +0500

    add decorator

---
+++ Decorator.py
@@ -0,0 +1,62 @@
+from abc import ABC, abstractmethod
+
+
+class Notifier(ABC):
+
+    @abstractmethod
+    def send(self):
+        pass
+
+
+class ConcreteNotifier(Notifier):
+
+    def send(self):
+        return 'Notifier: '
+
+
+class BaseDecorator(Notifier):
+
+    _notify = None
+
+    def __init__(self, notify):
+        self._notify = notify
+
+    def send(self):
+        self._notify.send()
+
+
+class InsideNetworkDecorator(BaseDecorator):
+
+    def send(self):
+        return self._notify.send() + 'The notification was sent to inside network'
+
+
+class OnEmailDecorator(BaseDecorator):
+
+    def send(self):
+        return self._notify.send() + 'The notification was sent to email'
+
+
+class OnTelegramDecorator(BaseDecorator):
+
+    def send(self):
+        return self._notify.send() + '\nThe notification was sent to Telegram'
+
+
+class OnWhatsAppDecorator(BaseDecorator):
+
+    def send(self):
+        return self._notify.send() + '\nThe notification was sent to WhatsApp'
+
+
+class OnViberDecorator(BaseDecorator):
+
+    def send(self):
+        return self._notify.send() + '\nThe notification was sent to Viber'
+
+
+if __name__ == '__main__':
+
+
+
+
